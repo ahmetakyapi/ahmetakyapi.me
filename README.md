b10ae075d7e405ed7953279655cdeb38e583cc29
ahmetakyapi <ahmetakyapii@gmail.com>
2021-04-07 21:30:47 +0300

    Proje başlangıcı yapıldı.

---
+++ README.md
@@ -0,0 +1,2 @@
+### Blog Sitesi Projesi
+
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function HomePage() {
+    return <div className="text text-purple-500 bg bg-blue-100">Welcome to Next.js!</div>
+}
+
+export default HomePage
