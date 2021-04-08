0cb743277ff9af3e8cc0d22bdb14034b2f7db9b2
ahmetakyapi <ahmetakyapii@gmail.com>
2021-04-09 00:18:45 +0300

    Giriş sayfası ve Layout tamamlandı.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
 function HomePage() {
-    return <div className="text text-purple-500 bg bg-blue-100">Welcome to Next.js!</div>
+  return (
+    <>
+      <div className="site-container">
+        <div className="space-y-4">
+          <h1 className="text-2xl font-bold">
+            Ben Ahmet Akyapı. Bilgisayar Mühendisliği Öğrencisiyim.
+          </h1>
+
+          <p>
+            Web tabanlı uygulamalar gelişmekte ve çeşitli projeler
+            oluşturmaktayım.
+          </p>
+
+          <p>
+            Burada projelerimi paylaşıp, yaptığım veya yapacağım içerikler
+            hakkında bilgileri paylaşacağım. Aşağıdaki linklerden bana
+            ulaşabilirsiniz.
+          </p>
+        </div>
+      </div>
+
+      <div className="max-w-4xl mx-auto mt-10">
+        <img src="/i-am.jpeg" alt="" />
+      </div>
+    </>
+  )
 }
 
 export default HomePage
