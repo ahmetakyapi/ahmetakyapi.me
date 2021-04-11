7c6fc9f1bf0e1dbf9a9b74a5ca104516a9c24479
ahmetakyapi <ahmetakyapii@gmail.com>
2021-04-11 23:56:36 +0300

    Blog yazıları ve detay sayfaları oluşturuldu.

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ function HomePage() {
         </div>
       </div>
 
-      <div className="max-w-4xl mx-auto mt-10">
+      <div className="site-4xl-container mt-20">
         <img src="/i-am.jpeg" alt="" />
       </div>
     </>
