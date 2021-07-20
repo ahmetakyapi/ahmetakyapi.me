e8d016ccbc8edb53aaf066e110f2f32ae110ca2d
ahmetakyapi <ahmetakyapii@gmail.com>
2021-07-21 00:02:13 +0300

    Add project details from pages

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -6,52 +6,195 @@ function ProjectsPage({ projects }) {
       </div>
       <div className="flex flex-wrap content-evenly place-content-between">
         <div className="card mt-6 mb-10">
-          <img src="https://picsum.photos/id/404/367/267" alt="image" />
-          <h3 className="text-center">Lorem ipsum</h3>
+          <img
+            src="projects/ahmetakyapi.me.png"
+            alt="tesla-clone-ahmet akyapı"
+          />
+          <h3 className="text-center font-bold text-indigo-900">
+            Ahmetakyapi.me
+          </h3>
           <div className="focus-content">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              <br /> <a href="#">Link to source</a>
+            <p className="text-center">
+              Next.js, Auth0, Redis ile oluşturduğum bu projenin kaynak
+              kodlarına Github üzerinden ulaşabilirsiniz.
+              <br />{' '}
+              <div className="text-center mt-2 text-green-500">
+                <a
+                  className="mr-16"
+                  href="https://github.com/ahmetakyapi/Tesla-Clone"
+                  target="_blank"
+                >
+                  Github Repo
+                </a>{' '}
+                <a
+                  href="https://tesla-clone-ahmetakyapi.vercel.app/"
+                  target="_blank"
+                >
+                  Demo
+                </a>
+              </div>
+            </p>
+          </div>
+        </div>
+        <div className="card mt-6 mb-10">
+          <img src="projects/Tesla-Clone.png" alt="tesla-clone-ahmet akyapı" />
+          <h3 className="text-center font-bold text-indigo-900">
+            React Tesla Clone
+          </h3>
+          <div className="focus-content">
+            <p className="text-center">
+              React ile, Tesla sitesinin ana sayfa clone projesi.
+              <br />{' '}
+              <div className="text-center mt-2 text-green-500">
+                <a
+                  className="mr-16"
+                  href="https://github.com/ahmetakyapi/Tesla-Clone"
+                  target="_blank"
+                >
+                  Github Repo
+                </a>{' '}
+                <a
+                  href="https://tesla-clone-ahmetakyapi.vercel.app/"
+                  target="_blank"
+                >
+                  Demo
+                </a>
+              </div>
             </p>
           </div>
         </div>
         <div className="card mt-6 mb-10">
           <img src="https://picsum.photos/id/404/367/267" alt="image" />
-          <h3 className="text-center">Lorem ipsum</h3>
+          <h3 className="text-center font-bold text-indigo-900">
+            React Tesla Clone
+          </h3>
           <div className="focus-content">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              <br /> <a href="#">Link to source</a>
+            <p className="text-center">
+              React ile, Tesla sitesinin ana sayfa clone projesi.
+              <br />{' '}
+              <div className="text-center mt-2 text-green-500">
+                <a
+                  className="mr-16"
+                  href="https://github.com/ahmetakyapi/Tesla-Clone"
+                  target="_blank"
+                >
+                  Github Repo
+                </a>{' '}
+                <a
+                  href="https://tesla-clone-ahmetakyapi.vercel.app/"
+                  target="_blank"
+                >
+                  Demo
+                </a>
+              </div>
             </p>
           </div>
         </div>
         <div className="card mt-6 mb-10">
           <img src="https://picsum.photos/id/404/367/267" alt="image" />
-          <h3 className="text-center">Lorem ipsum</h3>
+          <h3 className="text-center font-bold text-indigo-900">
+            React Tesla Clone
+          </h3>
           <div className="focus-content">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              <br /> <a href="#">Link to source</a>
+            <p className="text-center">
+              React ile, Tesla sitesinin ana sayfa clone projesi.
+              <br />{' '}
+              <div className="text-center mt-2 text-green-500">
+                <a
+                  className="mr-16"
+                  href="https://github.com/ahmetakyapi/Tesla-Clone"
+                  target="_blank"
+                >
+                  Github Repo
+                </a>{' '}
+                <a
+                  href="https://tesla-clone-ahmetakyapi.vercel.app/"
+                  target="_blank"
+                >
+                  Demo
+                </a>
+              </div>
             </p>
           </div>
         </div>
-        <div className="card mt-6 mb-12">
+        <div className="card mt-6 mb-10">
           <img src="https://picsum.photos/id/404/367/267" alt="image" />
-          <h3 className="text-center">Lorem ipsum</h3>
+          <h3 className="text-center font-bold text-indigo-900">
+            React Tesla Clone
+          </h3>
           <div className="focus-content">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              <br /> <a href="#">Link to source</a>
+            <p className="text-center">
+              React ile, Tesla sitesinin ana sayfa clone projesi.
+              <br />{' '}
+              <div className="text-center mt-2 text-green-500">
+                <a
+                  className="mr-16"
+                  href="https://github.com/ahmetakyapi/Tesla-Clone"
+                  target="_blank"
+                >
+                  Github Repo
+                </a>{' '}
+                <a
+                  href="https://tesla-clone-ahmetakyapi.vercel.app/"
+                  target="_blank"
+                >
+                  Demo
+                </a>
+              </div>
             </p>
           </div>
         </div>
-        <div className="card mt-6 mb-12">
+        <div className="card mt-6 mb-10">
           <img src="https://picsum.photos/id/404/367/267" alt="image" />
-          <h3 className="text-center">Lorem ipsum</h3>
+          <h3 className="text-center font-bold text-indigo-900">
+            React Tesla Clone
+          </h3>
           <div className="focus-content">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              <br /> <a href="#">Link to source</a>
+            <p className="text-center">
+              React ile, Tesla sitesinin ana sayfa clone projesi.
+              <br />{' '}
+              <div className="text-center mt-2 text-green-500">
+                <a
+                  className="mr-16"
+                  href="https://github.com/ahmetakyapi/Tesla-Clone"
+                  target="_blank"
+                >
+                  Github Repo
+                </a>{' '}
+                <a
+                  href="https://tesla-clone-ahmetakyapi.vercel.app/"
+                  target="_blank"
+                >
+                  Demo
+                </a>
+              </div>
+            </p>
+          </div>
+        </div>
+        <div className="card mt-6 mb-10">
+          <img src="https://picsum.photos/id/404/367/267" alt="image" />
+          <h3 className="text-center font-bold text-indigo-900">
+            React Tesla Clone
+          </h3>
+          <div className="focus-content">
+            <p className="text-center">
+              React ile, Tesla sitesinin ana sayfa clone projesi.
+              <br />{' '}
+              <div className="text-center mt-2 text-green-500">
+                <a
+                  className="mr-16"
+                  href="https://github.com/ahmetakyapi/Tesla-Clone"
+                  target="_blank"
+                >
+                  Github Repo
+                </a>{' '}
+                <a
+                  href="https://tesla-clone-ahmetakyapi.vercel.app/"
+                  target="_blank"
+                >
+                  Demo
+                </a>
+              </div>
             </p>
           </div>
         </div>
